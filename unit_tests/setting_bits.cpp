56ddb9e0e63262428f98d60c63a4568dc059a360
Tomasz Leman <apacz87@gmail.com>
2017-12-12 23:02:52 +0100

    Rename test case in 'setting_bits.cpp'

--- unit_tests/setting_bits.cpp
+++ unit_tests/setting_bits.cpp
@@ -1,13 +1,13 @@
 #include "gtest/gtest.h"
 #include "binary_operations.hpp"
 
-TEST(binary_operations, setbiton)
+TEST(setting_bits, setbiton)
 {
    auto res = bopr::setbiton(0x00000000, 1);
    EXPECT_EQ(0x00000002, res);
 }
 
-TEST(binary_operations, setbitoff)
+TEST(setting_bits, setbitoff)
 {
    auto res = bopr::setbitoff(0xFFFFFFFF, 1);
    EXPECT_EQ(0xFFFFFFFD, res);
