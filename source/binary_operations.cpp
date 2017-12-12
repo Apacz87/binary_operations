0e84837f267a321079a857656db3a5c91cca8764
Tomasz Leman <apacz87@gmail.com>
2017-12-12 22:57:50 +0100

    Add new functions into library

--- source/binary_operations.cpp
+++ source/binary_operations.cpp
@@ -27,4 +27,23 @@ namespace bopr
       mask = t_value & oBitState;
       return bitState | mask;
    }
+
+   unsigned invertbits(const unsigned& t_value, const int& t_last, const int& t_first)
+   {
+      unsigned bitsState, opposedBitState, mask;
+      bitsState = t_value >> t_last + 1 - t_first;
+      bitsState = ~bitsState;
+      bitsState &= ~(~0 << t_first);
+      bitsState <<= t_last + 1 - t_first;
+      opposedBitState = ~(~0 << t_first);
+      opposedBitState <<= t_last + 1 - t_first;
+      opposedBitState = ~opposedBitState;
+      mask = t_value & opposedBitState;
+      return bitsState | mask;
+   }
+
+   bool bitstate(const unsigned& t_value, const int& t_bit)
+   {
+      return ((t_value >> t_bit) & 1) != 0;
+   }
 }
--- source/binary_operations.hpp
+++ source/binary_operations.hpp
@@ -6,6 +6,8 @@ namespace bopr
    unsigned setbiton(const unsigned& t_value, const int& t_bit);
    unsigned setbitoff(const unsigned& t_value, const int& t_bit);
    unsigned invertbit(const unsigned& t_value, const int& t_bit);
+   unsigned invertbits(const unsigned& t_value, const int& t_last, const int& t_first);
+   bool bitstate(const unsigned& t_value, const int& t_bit);
 }
 
 #endif
