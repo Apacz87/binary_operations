9fb2013a3339ad5d1baa261e1754a05144171d40
Tomasz Leman <apacz87@gmail.com>
2017-12-18 20:59:55 +0100

    invert bits tests update

--- unit_tests/invert_bits.cpp
+++ unit_tests/invert_bits.cpp
@@ -3,12 +3,18 @@
 
 TEST(invert_bits, intertbit)
 {
-	 auto res = bopr::invertbit(0xFF125B89, 7);
-	 EXPECT_EQ(0xFF125B09, res);
+   auto res = bopr::invertbit(0xFF125B89, 7);
+   EXPECT_EQ(0xFF125B09, res);
+
+   res = bopr::invertbit(0xFF125B09, 13);
+   EXPECT_EQ(0xFF127B09, res);
 }
 
 TEST(invert_bits, invertbits)
 {
-	 auto res = bopr::invertbits(0xFF125B89, 20, 4);
-	 EXPECT_EQ(0xFF0C5B89, res);
+   auto res = bopr::invertbits(0xFF125B89, 20, 4);
+   EXPECT_EQ(0xFF0C5B89, res);
+
+   res = bopr::invertbits(0xFF127B09, 21, 8);
+   EXPECT_EQ(0xFF2DBB09, res);
 }
