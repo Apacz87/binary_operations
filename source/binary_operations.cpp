c51e87754551952db1c995be9898f9e1dbd42e7b
Tomasz Leman <apacz87@gmail.com>
2017-12-18 20:59:16 +0100

    invertbit function refactoring

--- source/binary_operations.cpp
+++ source/binary_operations.cpp
@@ -16,16 +16,9 @@ namespace bopr
 
    unsigned invertbit(const unsigned& t_value, const int& t_bit)
    {
-      unsigned bitState, oBitState, mask;
-      bitState = t_value >> t_bit;
-      bitState = ~bitState;
-      bitState &= ~(~0 << 1);
-      bitState <<= t_bit;
-      oBitState = ~(~0 << 1);
-      oBitState <<= t_bit;
-      oBitState = ~oBitState;
-      mask = t_value & oBitState;
-      return bitState | mask;
+      unsigned mask;
+      mask = 1 << t_bit;
+      return t_value ^ mask;
    }
 
    unsigned invertbits(const unsigned& t_value, const int& t_last, const int& t_first)
